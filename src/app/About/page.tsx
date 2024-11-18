93fbb82f2c79ef8279a4cbe5bc3b0f05290b5c1d
Warihakhan0 <futuretechai0@gmail.com>
2024-11-18 23:13:40 +0500

    push it

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -15,10 +15,10 @@ export default function About(){
           <p className="w-2/4 rounded-md text-purple-300 bg-purple-950 p-5">
 <br />
 <br />
-My journey in development began with an interest in understanding how the web works, and it quickly grew into a fulfilling career path. I've honed my skills in TypeScript, JavaScript, and various modern libraries and frameworks, including Next.js, React, and Inquirer.js. These tools allow me to build not only visually appealing but also high-performance applications that enhance user experience.
+My journey in development began with an interest in understanding how the web works, and it quickly grew into a fulfilling career path. I&apos;ve honed my skills in TypeScript, JavaScript, and various modern libraries and frameworks, including Next.js, React, and Inquirer.js. These tools allow me to build not only visually appealing but also high-performance applications that enhance user experience.
 <br />
 <br />
-Whether collaborating with teams or working independently, I’m always committed to producing high-quality results. When I’m not coding, you might find me exploring the latest tech trends, learning new languages, or sharing knowledge with others in the developer community. I'm excited to bring this energy and dedication to any project I work on!</p>
+Whether collaborating with teams or working independently, I&apos;m always committed to producing high-quality results. When I&apos;m not coding, you might find me exploring the latest tech trends, learning new languages, or sharing knowledge with others in the developer community. I&apos;m excited to bring this energy and dedication to any project I work on!</p>
           <button type="button" className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-white mt-5 w-2/4 p-2 rounded-md font-serif font-semibold text-purple-950 hover:bg-purple-950 hover:text-white">
           <a href="https://milestones-coral.vercel.app/view.html" target="_blank">View Resume</a></button>
           <div className="flex flex-row gap-5 my-5 text-3xl text-slate-300 mx-20">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         className="rounded-full flex mr-6 mb-14"/>
         <div className="flex flex-col text-left justify-center">
         <h1 className="text-6xl font-serif font-bold text-gray-100 mx-10">Wariha Khan</h1>
-        <p className="w-3/4 text-purple-300 my-6 mx-10">Welcome to my portfolio! I'm Wariha Khan, a passionate and skilled Web Developer. My goal is to create impactful digital experiences through clean, functional, and user-centered design. Here, you'll find a collection of my projects, from web applications and design prototypes to collaborative work and case studies.</p>
+        <p className="w-3/4 text-purple-300 my-6 mx-10">Welcome to my portfolio! I&apos;m Wariha Khan, a passionate and skilled Web Developer. My goal is to create impactful digital experiences through clean, functional, and user-centered design. Here, you&apos;ll find a collection of my projects, from web applications and design prototypes to collaborative work and case studies.</p>
         <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-purple-800 w-1/4 p-2 text-white font-semibold text-lg font-serif hover:bg-purple-950 mx-10"><a href="/Contact" target="_blank">Contact Me </a></button>
         </div>
       </div>
@@ -30,10 +30,10 @@ export default function Home() {
           <p className="w-2/4 rounded-md text-purple-300 bg-purple-950 p-5">
 <br />
 <br />
-My journey in development began with an interest in understanding how the web works, and it quickly grew into a fulfilling career path. I've honed my skills in TypeScript, JavaScript, and various modern libraries and frameworks, including Next.js, React, and Inquirer.js. These tools allow me to build not only visually appealing but also high-performance applications that enhance user experience.
+My journey in development began with an interest in understanding how the web works, and it quickly grew into a fulfilling career path. I&apos;ve honed my skills in TypeScript, JavaScript, and various modern libraries and frameworks, including Next.js, React, and Inquirer.js. These tools allow me to build not only visually appealing but also high-performance applications that enhance user experience.
 <br />
 <br />
-Whether collaborating with teams or working independently, I’m always committed to producing high-quality results. When I’m not coding, you might find me exploring the latest tech trends, learning new languages, or sharing knowledge with others in the developer community. I'm excited to bring this energy and dedication to any project I work on!</p>
+Whether collaborating with teams or working independently, I&apos;m always committed to producing high-quality results. When I&apos;m not coding, you might find me exploring the latest tech trends, learning new languages, or sharing knowledge with others in the developer community. I&apos;m excited to bring this energy and dedication to any project I work on!</p>
           <button type="button" className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-white mt-5 w-2/4 p-2 rounded-md font-serif font-semibold text-purple-950 hover:bg-purple-950 hover:text-white">
           <a href="https://milestones-coral.vercel.app/view.html" target="_blank">View Resume</a></button>
           <div className="flex flex-row gap-5 my-5 text-3xl text-slate-300 mx-20">
